d2660969cafb70bba8820482ce704c81b6c6a93f
olaf-snow <olaf.snow@navercorp.com>
2021-08-12 17:54:26 +0900

    feature: router page 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import logo from './logo.svg'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import MainPage from './pages/main'
+import ResultPage from './pages/result'
+import NotFoundPage from './pages/notFound'
+
 import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route path="/:id">
+          <ResultPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
